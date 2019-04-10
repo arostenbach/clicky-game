eb01908ff3e93b4f7e0ce770ff887bea931d876a
ar1234 <ashleyrostenbach@Ashleys-MacBook-Pro.local>
2019-04-10 08:40:53 -0700

    Adding Clicky-game

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="bottom">
+        Clicky Game! <img alt="react" src="assets/images/cali4.jpeg" />
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
